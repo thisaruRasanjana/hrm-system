02019dafe069ebffd2132d0e0a322a5081690e96
sachintha dilshan <sachintharcm@gmail.com>
2026-01-06 20:34:51 +0530

    backend

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -2,6 +2,7 @@ import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from urllib.parse import quote_plus
 
 load_dotenv()
 
@@ -11,10 +12,13 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
+
 DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}"
+    f"postgresql://{DB_USER}:{quote_plus(DB_PASSWORD)}"
     f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 )
+
+# print("Connecting to database at:", DATABASE_URL)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
