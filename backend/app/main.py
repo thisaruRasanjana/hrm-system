d3b320c81788f13dec85fae29305bd6b386a376e
Thisaru Ramanayaka <thisarurasanjana242@gmail.com>
2026-01-06 13:10:22 +0530

    add PostgreSQL database connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,13 @@
 from fastapi import FastAPI
+from app.database.database import engine
+
 app = FastAPI(title="HRM Backend")
+
+@app.on_event("startup")
+def startup():
+    with engine.connect() as connection:
+        pass
 
 @app.get("/")
 def root():
-    return {"message": "HRM backend is running"}
+    return {"message": "HRM backend with DB connected"}
