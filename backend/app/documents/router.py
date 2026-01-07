5040b1b95e39525640713f39ed4bcbb0abb74783
sachintha dilshan <sachintharcm@gmail.com>
2026-01-07 18:41:18 +0530

    document frontend

--- backend/app/documents/router.py
+++ backend/app/documents/router.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, Depends, UploadFile, File, Form
 from sqlalchemy.orm import Session
 from uuid import UUID
+from fastapi.responses import FileResponse
 
 from app.database.database import get_db
 from app.documents import service, schemas
-
-router = APIRouter(prefix="/documents", tags=["Documents"])
-
+router = APIRouter(
+    prefix="/documents",
+    tags=["Documents"]
+)
 @router.post("/upload", response_model=schemas.DocumentUploadResponse)
 def upload_document(
     employee_id: UUID = Form(...),
@@ -15,18 +17,30 @@ def upload_document(
     file: UploadFile = File(...),
     db: Session = Depends(get_db)
 ):
-    document = service.upload_employee_document(
+    return service.upload_employee_document(
         db=db,
         employee_id=employee_id,
         document_type=document_type,
         is_mandatory=is_mandatory,
         file=file
     )
-    return document
-
-@router.get("/my-documents", response_model=list[schemas.EmployeeDocumentResponse])
+@router.get(
+    "/my-documents",
+    response_model=list[schemas.EmployeeDocumentResponse]
+)
 def get_my_documents(
     employee_id: UUID,
     db: Session = Depends(get_db)
 ):
     return service.get_employee_documents(db, employee_id)
+@router.get("/download/{document_id}")
+def download_document(
+    document_id: UUID,
+    employee_id: UUID,
+    db: Session = Depends(get_db)
+):
+    return service.download_employee_document(
+        db=db,
+        document_id=document_id,
+        employee_id=employee_id
+    )
