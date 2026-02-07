51fad5495d798c90a050ceca462f3aa3aca21ba3
Thisaru Ramanayaka <thisarurasanjana242@gmail.com>
2026-02-07 14:35:45 +0530

    database adjustments during recruitment development

--- backend/app/database/database.py
+++ backend/app/database/database.py
@@ -11,10 +11,7 @@ DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-DATABASE_URL = (
-    f"postgresql://{DB_USER}:{DB_PASSWORD}"
-    f"@{DB_HOST}:{DB_PORT}/{DB_NAME}"
-)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(
