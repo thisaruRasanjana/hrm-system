e8c32cba4a159969090abf3ddb1534b941f63ac0
sachintha dilshan <sachintharcm@gmail.com>
2026-03-17 17:32:56 +0530

    ,,,

---
+++ frontend/app/hr/documents/request_management/page.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import DocumentTabsHR from "../../../components/DocumentTabsHR";
+
+export default function HRRequestDocumentsPage() {
+  return (
+    <div className="space-y-6">
+
+      <DocumentTabsHR />
+
+      <h1 className="text-2xl font-semibold">
+         approve Requested Documents
+      </h1>
+
+      
+
+    </div>
+  );
+}
