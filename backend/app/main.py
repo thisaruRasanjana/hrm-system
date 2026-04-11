4a9644651fca7b195aa417e01a73d16c5a3145ec
SanduniLiyanage <sanduniliyanage045@gmail.com>
2026-04-11 20:59:00 +0530

    feat: initialize FastAPI backend structure with database integration and VS Code workspace configuration

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,7 +33,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
-        "http://127.0.0.1:3000"
+        "http://127.0.0.1:3000",
+        "http://192.168.56.1:3000"
     ],
     allow_credentials=True,
     allow_methods=["*"],
